f4447df3e19eea3d0f43fcddad42067aad7582ce
Malitthh <malith.akalanka@outlook.com>
2022-10-29 00:15:00 +0530

    Update index.jsx

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   // set damage report data to usestate
   const [damageReportsData, setDamageReportsData] = useState([]);
   // get damage reports from state
-  const { damageReports, auth  } = useSelector((state) => state);
+  const { damageReports, auth } = useSelector((state) => state);
 
-  console.log(auth, "auth")
+  console.log(auth, "auth");
 
   // useEffect(() => {
   //   // get uuid's from state and loop it and read files in db folder
@@ -34,22 +34,129 @@ const Home = () => {
   return (
     <div className="min-h-full">
       <NavBar />
-      <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-          <div className="float-right">
-            <a data-cy="link-new-report" href="/reports/new-report" className="new-report btn btn-primary gap-2 btn-sm">
-                <i className="fa fa-plus" aria-hidden="true"></i>
-                New Report
-            </a>
-          </div>
-          <h1 className="text-1xl font-bold tracking-tight text-gray-900">
-            Dashboard
-          </h1>
-        </div>
-      </header>
+
+      <div className="container">
+        <nav className="biolife-nav">
+          <ul>
+            <li className="nav-item">
+              <a href="/admin" className="permal-link">
+                Dashboard
+              </a>
+            </li>
+            <li className="nav-item">
+              <span className="current-page">Dashboard</span>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          {/* <PreviousReports damageReportsData={damageReportsData} /> */}
+          <div className="container mx-2">
+            <div className="overflow-x-auto">
+              <div class="row">
+                <div class="col-xl-3 col-lg-6">
+                  <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col">
+                          <h5 class="card-title text-uppercase text-muted mb-0">
+                            Traffic
+                          </h5>
+                          <span class="h2 font-weight-bold mb-0">350,897</span>
+                        </div>
+                        <div class="col-auto">
+                          <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                            <i class="fas fa-chart-bar"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-success mr-2">
+                          <i class="fa fa-arrow-up"></i> 3.48%
+                        </span>
+                        <span class="text-nowrap">Since last month</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                  <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col">
+                          <h5 class="card-title text-uppercase text-muted mb-0">
+                            New users
+                          </h5>
+                          <span class="h2 font-weight-bold mb-0">2,356</span>
+                        </div>
+                        <div class="col-auto">
+                          <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                            <i class="fas fa-chart-pie"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-danger mr-2">
+                          <i class="fas fa-arrow-down"></i> 3.48%
+                        </span>
+                        <span class="text-nowrap">Since last week</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                  <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col">
+                          <h5 class="card-title text-uppercase text-muted mb-0">
+                            Sales
+                          </h5>
+                          <span class="h2 font-weight-bold mb-0">924</span>
+                        </div>
+                        <div class="col-auto">
+                          <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                            <i class="fas fa-users"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-warning mr-2">
+                          <i class="fas fa-arrow-down"></i> 1.10%
+                        </span>
+                        <span class="text-nowrap">Since yesterday</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                  <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col">
+                          <h5 class="card-title text-uppercase text-muted mb-0">
+                            Performance
+                          </h5>
+                          <span class="h2 font-weight-bold mb-0">49,65%</span>
+                        </div>
+                        <div class="col-auto">
+                          <div class="icon icon-shape bg-info text-white rounded-circle shadow">
+                            <i class="fas fa-percent"></i>
+                          </div>
+                        </div>
+                      </div>
+                      <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-success mr-2">
+                          <i class="fas fa-arrow-up"></i> 12%
+                        </span>
+                        <span class="text-nowrap">Since last month</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
