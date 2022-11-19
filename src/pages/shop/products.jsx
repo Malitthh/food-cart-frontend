c9b8590afb4fe3274f042dbe6d1450bf8ea197c1
Malitthh <malith.akalanka@outlook.com>
2022-11-19 11:30:55 +0530

    Update products.jsx

--- src/pages/shop/products.jsx
+++ src/pages/shop/products.jsx
@@ -11,7 +11,7 @@ const product = () => {
   const dispatch = useDispatch();
   const { products, auth } = useSelector((state) => state);
   const { allProducts } = products;
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   const featchOnLoad = async () => {
@@ -23,14 +23,14 @@ const product = () => {
   }, []);
 
   const currentCategory = (category) => {
-    setCategory(category)
-    setShowAll(false)
-  }
+    setCategory(category);
+    setShowAll(false);
+  };
 
   const resetCategory = () => {
-    setCategory('')
-    setShowAll(true)
-  }
+    setCategory("");
+    setShowAll(true);
+  };
 
   return (
     <>
@@ -59,38 +59,107 @@ const product = () => {
       </div>
       <div className="container">
         <div className="row">
-          <div className="col-md-3">     
-          <aside id="sidebar" class="sidebar col-lg-8 col-md-4 col-sm-12 col-xs-12">
-                    <div class="biolife-mobile-panels">
-                        <span class="biolife-current-panel-title">Sidebar</span>
-                        <a class="biolife-close-btn" href="#" data-object="open-mobile-filter">&times;</a>
-                    </div>
-                    <div class="sidebar-contain">
-                        <div class="widget biolife-filter">
-                            <h3 class="wgt-title"><b>Categories List</b></h3>
-                            <div class="wgt-content">
-                                <ul class="cat-list">
-                                    <li class="cat-list-item"><a style={{ cursor: 'pointer'}} onClick={() => currentCategory('fruit_nuts')} class="cat-link">Fruits & Nuts</a></li>
-                                    <li class="cat-list-item"><a style={{ cursor: 'pointer'}} onClick={() => currentCategory('vegetables')} class="cat-link">Vegetables</a></li>
-                                    <li class="cat-list-item"><a style={{ cursor: 'pointer'}} onClick={() => currentCategory('butter_eggs')} class="cat-link">Butter & Eggs</a></li>
-                                    <li class="cat-list-item"><a style={{ cursor: 'pointer'}} onClick={() => currentCategory('berries')} class="cat-link">Berries</a></li>
-                                    <li class="cat-list-item"><a style={{ cursor: 'pointer'}} onClick={() => currentCategory('rice')} class="cat-link">Rice</a></li>
-                                    <li class="cat-list-item"><a style={{ cursor: 'pointer'}} onClick={() => resetCategory()} class="cat-link">Reset and All categories</a></li>
-                                </ul>
-                            </div>
-                        </div>     
-                    </div>
-                </aside>
+          <div className="col-md-3">
+            <aside
+              id="sidebar"
+              class="sidebar col-lg-8 col-md-4 col-sm-12 col-xs-12"
+            >
+              <div class="biolife-mobile-panels">
+                <span class="biolife-current-panel-title">Sidebar</span>
+                <a
+                  class="biolife-close-btn"
+                  href="#"
+                  data-object="open-mobile-filter"
+                >
+                  &times;
+                </a>
+              </div>
+              <div class="sidebar-contain">
+                <div class="widget biolife-filter">
+                  <h3 class="wgt-title">
+                    <b>Categories List</b>
+                  </h3>
+                  <div class="wgt-content">
+                    <ul class="cat-list">
+                      <li class="cat-list-item">
+                        <a
+                          style={{ cursor: "pointer" }}
+                          onClick={() => currentCategory("fruit_nuts")}
+                          class="cat-link"
+                        >
+                          Fruits & Nuts
+                        </a>
+                      </li>
+                      <li class="cat-list-item">
+                        <a
+                          style={{ cursor: "pointer" }}
+                          onClick={() => currentCategory("vegetables")}
+                          class="cat-link"
+                        >
+                          Vegetables
+                        </a>
+                      </li>
+                      <li class="cat-list-item">
+                        <a
+                          style={{ cursor: "pointer" }}
+                          onClick={() => currentCategory("butter_eggs")}
+                          class="cat-link"
+                        >
+                          Butter & Eggs
+                        </a>
+                      </li>
+                      <li class="cat-list-item">
+                        <a
+                          style={{ cursor: "pointer" }}
+                          onClick={() => currentCategory("berries")}
+                          class="cat-link"
+                        >
+                          Berries
+                        </a>
+                      </li>
+                      <li class="cat-list-item">
+                        <a
+                          style={{ cursor: "pointer" }}
+                          onClick={() => currentCategory("rice")}
+                          class="cat-link"
+                        >
+                          Rice
+                        </a>
+                      </li>
+                      <li class="cat-list-item">
+                        <a
+                          style={{ cursor: "pointer" }}
+                          onClick={() => resetCategory()}
+                          class="cat-link"
+                        >
+                          Reset and All categories
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </aside>
           </div>
           <div className="col-md-9">
             <div className="row">
-              {showAll && allProducts && allProducts.map((product, key) => <ShopItem key={key} product={product}/>)}
-              {!showAll && allProducts && allProducts.filter((product) => product.category === category).map((product, key) => <ShopItem key={key} product={product}/>)}
+              {showAll &&
+                allProducts &&
+                allProducts.map((product, key) => (
+                  <ShopItem key={key} product={product} />
+                ))}
+              {!showAll &&
+                allProducts &&
+                allProducts
+                  .filter((product) => product.category === category)
+                  .map((product, key) => (
+                    <ShopItem key={key} product={product} />
+                  ))}
             </div>
           </div>
         </div>
-      </div> 
-  
+      </div>
+
       <Footer />
     </>
   );
